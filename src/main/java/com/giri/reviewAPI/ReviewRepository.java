439fb3a5fe6aca916971304b0845417b7633ccde
AJJU GIRI <93632064+Ajjuajith14@users.noreply.github.com>
2024-03-19 23:26:50 +0530

    Add files via upload

---
+++ src/main/java/com/giri/reviewAPI/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.giri.reviewAPI;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ src/test/java/com/giri/reviewAPI/ReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giri.reviewAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
